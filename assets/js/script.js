5a37ac133cd7ddc37cd1099dbb3207e9c90ec8fb
PedrooLska <pedrolucas81@hotmail.com>
2020-07-26 12:02:22 -0300

    Add Bem Architeture e Refatoração de Código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
-class BuscaCep {
-    constructor() {
-      this.result = document.querySelector('.result');
-      this.cep = document.querySelector('.cep');
-  
-      this.cors = 'https://cors-anywhere.herokuapp.com/';
-    }
-  
-    getCep() {
-      if(this.cep.value === '') {
-        this.clearResult();
-        this.errorMsg('Por favor preencha o campo.')
-      } else {
-        fetch(`${this.cors}viacep.com.br/ws/${this.cep.value}/json/`)
-        .then(response => response.json())
-        .then(data => {
-          let { cep, logradouro, bairro, localidade, uf } = data;
-          this.showResult(logradouro, bairro, localidade, uf, cep)
-        })
-        .catch(error => {
-          this.clearResult();
-          this.errorMsg('Cep Incorreto')
-        })
-        this.clearResult();
-        this.errorMsg('');
-      }
-    }
-  
-    showResult(logradouro, bairro, localidade, uf, cep) {
-      this.result.innerHTML = `${logradouro} - ${bairro}<br>
-       ${localidade}/${uf} - ${cep}`
-    }
-  
-    clearResult() {
-      this.result.innerHTML = '';
-      this.cep.value = '';
-      this.cep.focus();
-    }
-  
-    errorMsg(msg) {
-      let erro = document.querySelector('.erro');
-      erro.innerHTML = `${msg}`
-    }
+const result = document.querySelector('#result');
+const cep = document.querySelector('#cep');
+const cors = 'https://cors-anywhere.herokuapp.com/';
+const erro = document.querySelector('#erro');
+
+const getCep = () => {
+  if (cep.value === '') {
+    clearResult();
+    errorMsg('Por favor preencha o campo.')
+  } else {
+    fetch(`${cors}viacep.com.br/ws/${cep.value}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        let {
+          cep,
+          logradouro,
+          bairro,
+          localidade,
+          uf
+        } = data;
+        showResult(logradouro, bairro, localidade, uf, cep)
+      })
+      .catch(() => {
+        clearResult();
+        errorMsg('Cep Incorreto')
+      })
+    clearResult();
+    errorMsg('');
   }
-  
-  
-  const newBuscaCep = new BuscaCep();
+}
+
+const showResult = (logradouro, bairro, localidade, uf, cep) => {
+  result.innerHTML = `${logradouro} - ${bairro}<br>
+       ${localidade}/${uf} - ${cep}`
+}
+
+const clearResult = () => {
+  result.style.display = 'block'
+  result.innerHTML = '';
+  cep.value = '';
+  cep.focus();
+}
+
+const errorMsg = (msg) => {
+  erro.style.display = 'block'
+  erro.innerHTML = `${msg}`
+}
